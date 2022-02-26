06948060af3d47d2a286994f5a4a4fcae33c7618
MelloFello <88409500+Mello-Fello@users.noreply.github.com>
2022-02-25 19:43:11 -0900

    Version 1!

---
+++ gen.py
@@ -0,0 +1,26 @@
+import random
+import time
+
+#variables
+length = 150
+choice_index = [1, 2, 3, 4]
+choice = ['[]', '  []', '    []', '      []', '        []', '        []', '            []', '              []',]
+
+input('GenMania by Mello#4975\n\nPress enter to start.')
+for i in range(length):
+    rd_choice = random.choice(choice)
+    print(rd_choice)
+
+
+#for i in range(length):
+#    random.choice(choice_index)
+ #   print(choice_index)
+  #  if choice_index == 1:
+   #     print(choice[1])
+#    elif choice_index == 2:
+ #       print(choice[2])
+  #  elif choice_index == 3:
+   #     print(choice[3])
+    #elif choice_index == 4:
+    #    print(choice[4])
+
